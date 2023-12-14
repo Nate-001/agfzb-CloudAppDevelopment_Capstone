40a0d4a6e9fdc21fb1e032e2ea8646bebccae57e
Nate-001 <nathanieldross1@gmail.com>
2023-12-13 20:39:39 -0500

    Login and authentication and more features added to the app.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from .views import about, contact, get_dealerships 
+from .views import about, contact, get_dealerships, login_request, logout_request, registration_request
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -17,10 +17,13 @@ urlpatterns = [
     path(route='contact/', view=contact, name='contact'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
